7964e538a827942215f52148a145d98f6a2e4ff1
shashank shandilya v <shashank.v.shandilya@gmail.com>
2026-02-03 23:21:20 -0700

    feat: add input primitives and TextInput composed component
    Add composable form input components using Vue provide/inject pattern:
    - InputRoot: Context provider for form field state
    - InputField: Text input with prefix/suffix slots
    - InputError: Error message (shown when invalid)
    - InputHelp: Help text (shown when valid)
    - TextInput: Composed component bundling all primitives with Label

---
+++ apps/storybook/src/components/ui/InputHelp/InputHelp.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+import { INPUT_INJECTION_KEY } from '@/components/ui/InputRoot'
+
+const context = inject(INPUT_INJECTION_KEY)
+if (!context) {
+  throw new Error('InputHelp must be used within InputRoot')
+}
+</script>
+
+<template>
+  <div
+    v-if="!context.invalid.value"
+    :id="context.helpId.value"
+    data-slot="input-help"
+    class="form-text mt-space-xxxs fs-small"
+  >
+    <slot />
+  </div>
+</template>
